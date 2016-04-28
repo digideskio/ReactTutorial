575ba593e041e06e845c313cda5fbe097786a5c0
Stephen Coogan <stephen.coogan@paddypower.com>
2016-04-28 12:05:07 +0100

    Section 2, Lecture 28

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBALhAAmpEtDWR8NE8kdRKksYARSGhO_og';
 
@@ -25,6 +26,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
